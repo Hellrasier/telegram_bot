d9ae2fdaa907873b0f32aeba0123fe3dd2e44d38
Hellrasier <denislolipop48@gmail.com>
2021-05-20 14:41:31 +0300

    make it better

--- bot/bot_api.js
+++ bot/bot_api.js
@@ -34,53 +34,53 @@ module.exports = async () => {
         `, main_menu())
     })
 
-    // bot.hears('Start daily test', async ctx => {
-    //    try{
-    //         ctx.reply("So be ready.\nYou need to answer on 10 questions, and I'll check it and put you a mark")
-    //         const db_data = await db.get()
-    //         const tasks = shuffle(db_data).slice(0, 10).map(task => addKeyboard(task))
-    //         let corr_answers = 0
-    //         cache[ctx.from.id] = {
-    //             tasks: tasks,
-    //             number: 1,
-    //             correct_answered: 0
-    //         }
-    //         ctx.replyWithHTML(`<b>Question ${tasks.indexOf(tasks[0]) + 1}.</b> ${tasks[0].question}`, tasks[0].keyboard())
-    //    } catch(e) {
-    //        console.log(e)
-    //    }
-    // })
+    bot.hears('Start daily test', async ctx => {
+       try{
+            ctx.reply("So be ready.\nYou need to answer on 10 questions, and I'll check it and put you a mark")
+            const db_data = await db.get()
+            const tasks = shuffle(db_data).slice(0, 10).map(task => addKeyboard(task))
+            let corr_answers = 0
+            cache[ctx.from.id] = {
+                tasks: tasks,
+                number: 1,
+                correct_answered: 0
+            }
+            ctx.replyWithHTML(`<b>Question ${tasks.indexOf(tasks[0]) + 1}.</b> ${tasks[0].question}`, tasks[0].keyboard())
+       } catch(e) {
+           console.log(e)
+       }
+    })
 
-    // bot.action('yes', ctx => {
-    //     let {tasks, number} = cache[ctx.from.id]
-    //     ctx.replyWithHTML(`<b>Good job!</b> Correct answer is "${tasks[number-1].variants[tasks[number-1].correct]}"`)
-    //     cache[ctx.from.id].number++
-    //     number++
-    //     setTimeout(() => {
-    //         if (number <= 10) {
-    //             cache[ctx.from.id].correct_answered++
-    //             ctx.replyWithHTML(`<b>Question ${number}.</b> ${tasks[number-1].question}`, tasks[number-1].keyboard())
-    //             ctx.editMessageReplyMarkup()
-    //         } else {
-    //             ctx.replyWithHTML(`<b>Test is over, well done!</b> Your result is <b>${cache[ctx.from.id].correct_answered}/10</b>`)
-    //             ctx.editMessageReplyMarkup()
-    //         }
-    //     }, 50)
-    // })
+    bot.action('yes', ctx => {
+        let {tasks, number} = cache[ctx.from.id]
+        ctx.replyWithHTML(`<b>Good job!</b> Correct answer is "${tasks[number-1].variants[tasks[number-1].correct]}"`)
+        cache[ctx.from.id].number++
+        number++
+        setTimeout(() => {
+            if (number <= 10) {
+                cache[ctx.from.id].correct_answered++
+                ctx.replyWithHTML(`<b>Question ${number}.</b> ${tasks[number-1].question}`, tasks[number-1].keyboard())
+                ctx.editMessageReplyMarkup()
+            } else {
+                ctx.replyWithHTML(`<b>Test is over, well done!</b> Your result is <b>${cache[ctx.from.id].correct_answered}/10</b>`)
+                ctx.editMessageReplyMarkup()
+            }
+        }, 50)
+    })
 
-    // bot.action('no', ctx => {
-    //     let {tasks, number} = cache[ctx.from.id]
-    //     ctx.replyWithHTML(`<b>Wrong!</b> Correct answer is "${tasks[number-1].variants[tasks[number-1].correct]}"`)
-    //     cache[ctx.from.id].number = number + 1
-    //     number++
-    //     if (number <= 10) {
-    //         ctx.replyWithHTML(`<b>Question ${number}.</b> ${tasks[number-1].question}`, tasks[number-1].keyboard())
-    //         ctx.editMessageReplyMarkup()
-    //     } else {
-    //         ctx.replyWithHTML(`<b>Test is over, well done!</b> Your result is <b>${cache[ctx.from.id].correct_answered}/10</b>`)
-    //         ctx.editMessageReplyMarkup()
-    //     }
-    // })
+    bot.action('no', ctx => {
+        let {tasks, number} = cache[ctx.from.id]
+        ctx.replyWithHTML(`<b>Wrong!</b> Correct answer is "${tasks[number-1].variants[tasks[number-1].correct]}"`)
+        cache[ctx.from.id].number = number + 1
+        number++
+        if (number <= 10) {
+            ctx.replyWithHTML(`<b>Question ${number}.</b> ${tasks[number-1].question}`, tasks[number-1].keyboard())
+            ctx.editMessageReplyMarkup()
+        } else {
+            ctx.replyWithHTML(`<b>Test is over, well done!</b> Your result is <b>${cache[ctx.from.id].correct_answered}/10</b>`)
+            ctx.editMessageReplyMarkup()
+        }
+    })
 
     bot.launch()
 
