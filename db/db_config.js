07da27d9117523ef5678cc77268115cc82d401b2
Hellrasier <denislolipop48@gmail.com>
2021-05-20 13:32:57 +0300

    make it better

--- db/db_config.js
+++ db/db_config.js
@@ -1,9 +1,8 @@
 const {Pool} = require('pg')
 
 module.exports = new Pool({
-    host: '127.0.0.1',
-    port: 5432,
-    database: 'telegram_bot_tasks',
-    user: 'postgres',
-    password: 'qwerty4147628'
+    connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 })
