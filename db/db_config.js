7fd22638ceecf4ce539ca9eca0f8ec2b34fd2ac5
Hellrasier <denislolipop48@gmail.com>
2021-05-29 02:11:27 +0300

    aws database

--- db/db_config.js
+++ db/db_config.js
@@ -1,8 +1,10 @@
 const {Pool} = require('pg')
+require('dotenv').config()
 
 module.exports = new Pool({
-    connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+    user: process.env.DATABASE_USER,
+    host: process.env.DATABASE_HOST,
+    database: 'telegram_bot',
+    password: process.env.DATABASE_PASSWORD,
+    port: process.env.DATABASE_PORT
 })
